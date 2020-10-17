bdf763358c3bffd991cf101d159537a560072708
Sawan Kumar Jha <sawanjha2004@gmail.com>
2020-10-17 13:59:54 +0530

    Updates

---
+++ precache-manifest.748c3e7b114e456c29a508e774bb8aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429b23b137451e166460e70c7f8f104e",
+    "url": "/CovidWeb/index.html"
+  },
+  {
+    "revision": "c62569fe669828b758e1",
+    "url": "/CovidWeb/static/css/main.312b6668.chunk.css"
+  },
+  {
+    "revision": "e511de5241c983154abc",
+    "url": "/CovidWeb/static/js/2.7adb4d3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CovidWeb/static/js/2.7adb4d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62569fe669828b758e1",
+    "url": "/CovidWeb/static/js/main.defe4b93.chunk.js"
+  },
+  {
+    "revision": "1d0d527a8bbe7c6ce636",
+    "url": "/CovidWeb/static/js/runtime-main.ed5272e6.js"
+  }
+]);
